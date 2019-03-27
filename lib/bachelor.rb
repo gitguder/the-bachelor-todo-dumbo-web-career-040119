1681f768fde5875c65d6a41b299b103192aae8ab
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 21:29:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,6 +37,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     total_age += contestant["age"].to_i
   end
+  
   average = total_age.to_f/data[season]
   average.round
 end
